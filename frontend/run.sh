c19e56f2576f81994cdf3a454c9858397357884c
Denis <Hixon10@yandex.ru>
2026-02-07 15:31:38 -0800

    Call get_trx_balance from web ui

--- frontend/run.sh
+++ frontend/run.sh
@@ -2,5 +2,6 @@
 
 set -e
 
+cargo build -p frontend --target wasm32-unknown-unknown
 wasm-pack build --target web
 python3 -m http.server 8000
--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,6 +1,43 @@
+use std::process;
+use std::time::Duration;
+use usdt_wallet_core::tron_client::TrongridClient;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub async fn fetch_html(wallet: String) -> Result<String, JsValue> {
-    Ok(format!("test1--{wallet}"))
+pub async fn get_trx_balance(wallet: String, trongrid_api_key: String) -> Result<String, JsValue> {
+    let api_key_opt: Option<String> = {
+        let s = trongrid_api_key.trim();
+        if s.is_empty() {
+            None
+        } else {
+            Some(s.to_owned())
+        }
+    };
+
+    let client = TrongridClient::new(
+        "https://api.trongrid.io",
+        api_key_opt,
+        Duration::from_secs(5),
+    )
+    .unwrap_or_else(|err| {
+        // Print to stderr
+        eprintln!("Error creating TrongridClient: {err:?}");
+        // Exit with non-zero status code
+        process::exit(1);
+    });
+
+    let trx_balance = client
+        .get_trx_balance(wallet.as_str())
+        .await
+        .unwrap_or_else(|err| {
+            // Print to stderr
+            eprintln!("Error fetching trx balance: {err:?}");
+            // Exit with non-zero status code
+            process::exit(1);
+        });
+
+    #[allow(clippy::cast_precision_loss)]
+    let trx: f64 = trx_balance as f64 / 1_000_000.0;
+
+    Ok(format!("test3--{trx}"))
 }
